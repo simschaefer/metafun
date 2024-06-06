a2aa943602f0af55ca3c492881c0055bebfb6103
simschaefer <ssimonbe@students.uni-mainz.de>
2024-06-06 21:51:40 +0200

    fixing function

--- R/sim_meta.R
+++ R/sim_meta.R
@@ -14,6 +14,7 @@
 #' @export
 #' @importFrom psych fisherz
 #' @importFrom metafor escalc
+#' @importFrom faux rnorm_multi
 #' @importFrom dplyr summarise bind_rows group_by %>% mutate sym tibble rename
 #' @examples
 #' sim_meta(min_obs = 100, max_obs = 500, n_studies = 20, es_true = 0.7)
