87a1b6b45cf8718d0acf80f954bd4ac2104e8e63
Administrator <administrator@psyml201.Sowi.Uni-Mainz.DE>
2024-12-23 10:56:53 +0100

    include ZCOR into sim_metareg

--- R/sim_metareg.R
+++ R/sim_metareg.R
@@ -5,6 +5,7 @@
 #' @param n_studies number of studies
 #' @param intercept effect size (Hedges' g) without moderation effect
 #' @param random choose TRUE to add variability of true effect size
+#' @param effect_size choose effect size measure ('SMD' or 'ZCOR')
 #' @param tau standard deviation of true effect size
 #' @param formula specify metaregression formula
 #' @param mod_types types of predictor variables (categorical = 'cat', continuous = 'cont')
@@ -22,17 +23,32 @@
 #' mod_types = c('cat', 'cont'))
 
 sim_metareg <- function(min_obs = 100,
-                     max_obs = 500,
-                     n_studies = 100,
-                     intercept = 0.5,
-                     random = FALSE,
-                     tau = 0.01,
-                     formula,
-                     mod_types){
+                        max_obs = 500,
+                        n_studies = 100,
+                        intercept = 0.5,
+                        random = FALSE,
+                        effect_size = 'ZCOR',
+                        tau = 0.01,
+                        formula,
+                        mod_types){
 
   if(min_obs > max_obs){
     warning('Minimum number of observations is lower than maximum. Please adjust `min_obs` and `max_obs`.')
   }
+
+  # min_obs = 100
+  # max_obs = 500
+  # n_studies = 100
+  # intercept = 0.9
+  # random = FALSE
+  # effect_size = 'ZCOR'
+  # tau = 0.01
+  # formula = formula("y ~ 0.1*group + 0.05*quality + 0.2*control")
+  # mod_types = c('cat', 'cont', 'cat')
+  #
+
+  formula_parts = formula("y ~ 0.1*group + 0.05*quality + 0.2*control")
+  mod_types = c('cat', 'cont', 'cat')
 
   ### MODERATION EFFECTS ###
 
@@ -59,11 +75,15 @@ sim_metareg <- function(min_obs = 100,
     A[predictors[i]] <- if(mod_types[i] == 'cat'){
       rbinom(n_studies, 1, prob = 0.5)
     }else if(mod_types[i] == 'cont'){
-        rnorm(n_studies)
-      }
+      rnorm(n_studies)
+    }
   }
 
-  A['hedges_g'] <- A['intercept'] + as.matrix(A[predictors]) %*% beta
+  if(effect_size == 'ZCOR'){
+    A['intercept'] <- fisherz(A['intercept'])
+  }
+
+  A['es'] <- A['intercept'] + as.matrix(A[predictors]) %*% beta
 
   A_df <- as.data.frame(A)
 
@@ -77,33 +97,60 @@ sim_metareg <- function(min_obs = 100,
 
   A_df$n <- n
 
-  for(i in 1:nrow(A_df)){
+  if(effect_size == 'SMD'){
+    for(i in 1:nrow(A_df)){
 
-    random <- rnorm(A_df$n[i], mean = A_df$hedges_g[i])
-    sd <- sd(random)
-    m <- mean(random)
+      random <- rnorm(A_df$n[i], mean = A_df$es[i])
+      sd <- sd(random)
+      m <- mean(random)
 
-    A_df$m[i] <- m
-    A_df$se[i] <- sd/sqrt(A_df$n[i])
+      A_df$m[i] <- m
+      A_df$se[i] <- sd/sqrt(A_df$n[i])
 
+    }
+
+  }else if(effect_size == 'ZCOR'){
+    for(i in 1:nrow(A_df)){
+
+      random <- rnorm(A_df$n[i], mean = A_df$es[i], sd = 1/sqrt(A_df$n[i] - 3))
+      sd <- sd(random)
+      m <- mean(random)
+
+      A_df$m[i] <- m
+      A_df$se[i] <- 1/sqrt(A_df$n[i] - 3)
+
+    }
   }
 
   A_df <- escalc(data = A_df,
-         yi = m,
-         ni = n,
-         sei = se,
-         measure = 'SMD')
+                 yi = m,
+                 ni = n,
+                 sei = se,
+                 measure = effect_size)
 
   new_names <- str_replace_all(predictors, " ", "")
-  old_names <- paste0('X.', new_names)
+  old_names <- c(paste0('X.', new_names), 'es')
+
+  if(effect_size == 'SMD'){
+    new_name_es <- 'hedges_g'
+  }else if(effect_size == 'ZCOR'){
+    new_name_es <- 'z'
+  }
+
+  new_names <- c(new_names, new_name_es)
 
   A_df <- A_df %>%
-    rename(!!!setNames(old_names, new_names)) %>%
     mutate(study = 1:nrow(A_df)) %>%
-    select(study, hedges_g, n, se, intercept, everything()) %>%
+    select(study, es, n, se, intercept, everything()) %>%
+    rename(!!!setNames(old_names, new_names)) %>%
     select(-m) %>%
     tibble()
 
+  if(effect_size == 'ZCOR'){
+    A_df <- A_df %>%
+      mutate(r = fisherz2r(z)) %>%
+      select(study, z,r, everything())
+  }
 
   return(A_df)
 
